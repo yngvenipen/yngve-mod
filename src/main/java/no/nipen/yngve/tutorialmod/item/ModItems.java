2c83a11f2129a761281be4b5ca3e80b52da3554d
Yngve Midtun Nipen <yngve@nipen.no>
2022-10-30 09:50:12 +0100

    Scythe er nå handheld

--- src/main/java/no/nipen/yngve/tutorialmod/item/ModItems.java
+++ src/main/java/no/nipen/yngve/tutorialmod/item/ModItems.java
@@ -1,6 +1,8 @@
 package no.nipen.yngve.tutorialmod.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +14,7 @@ public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
     public static final RegistryObject<Item> SCYTHE = ITEMS.register("scythe",
-            () -> new Item(
+            () -> new SwordItem(Tiers.NETHERITE, 10, 1.6f,
                     new Item.Properties()
                             .stacksTo(1)
                             .tab(ModCreativeModeTab.TUTORIAL_TAB)
